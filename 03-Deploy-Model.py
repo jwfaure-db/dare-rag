df47c8925fb659dbe2997a5e3e98f1bd36088844
wirjo <daniel@wirjo.com>
2024-04-30 09:58:35 +1000

    Bar-raise 03-Deploy-Model.py

--- 03-Deploy-Model.py
+++ 03-Deploy-Model.py
@@ -1,24 +1,19 @@
 # Databricks notebook source
 # MAGIC %md-sandbox
-# MAGIC # Lab 3 Deploy your model with inference tables
+# MAGIC # Lab 3: Deploy and monitor
 # MAGIC
-# MAGIC
+# MAGIC Let's now deploy our model. Once live, you monitor its behavior to detect potential anomalies and drift over time.
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## 3.1 Getting started
-# MAGIC To get started, install Python libraries, and restart the kernel to use updated packages.
+# MAGIC ## 3.1 Install dependencies and run helper code
+# MAGIC First, install Python libraries, and restart the kernel to use updated packages.
 
 # COMMAND ----------
 
 # MAGIC %pip install databricks-sdk==0.18.0 mlflow==2.10.1
 # MAGIC dbutils.library.restartPython()
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC Let's run the init notebook, which imports helpers for the lab.
 
 # COMMAND ----------
 
@@ -29,11 +24,10 @@
 # MAGIC %md-sandbox
 # MAGIC ## 3.2 Deploy our model with Inference tables
 # MAGIC
-# MAGIC <img src="https://github.com/databricks-demos/dbdemos-resources/blob/main/images/product/chatbot-rag/rag-eval-online-2-1.png?raw=true" style="float: right" width="900px">
+# MAGIC <img src="https://github.com/databricks-demos/dbdemos-resources/blob/main/images/product/chatbot-rag/rag-eval-online-2-1.png?raw=true" style="display: block; margin: 25px auto;" width="900px">
 # MAGIC
-# MAGIC Let's start deploying your model serving endpoint.
 # MAGIC
-# MAGIC 1. Let's first import some pre-requisite libraries.
+# MAGIC 1. Let's first import some prerequisite libraries.
 
 # COMMAND ----------
 
@@ -47,17 +41,16 @@ from databricks.sdk.service.serving import EndpointCoreConfigInput, ServedModelI
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 2. Let's configure the variables for the model to deploy.
+# MAGIC 2. Then, let's configure the variables for the model to deploy.
 # MAGIC
-# MAGIC Enable inference tables on model serving endpoints for monitoring and debugging
-# MAGIC The auto_capture_config parameter defines the table where the endpoint request payload will automatically be saved. Databricks will fill the table for you in the background, as a fully managed service.
+# MAGIC Enable [inference tables](https://docs.databricks.com/en/machine-learning/model-serving/inference-tables.html) on model serving endpoints for monitoring and debugging. The `auto_capture_config` parameter defines the table where the endpoint request payload will automatically be saved. Databricks will fill the table for you in the background, as a fully managed service.
 
 # COMMAND ----------
 
 environment_vars = {"DATABRICKS_TOKEN": dbutils.secrets.get(
     scope_name, "rag_sp_token")}
 model_name = f"{catalog}.{db}.advanced_chatbot_model"
-serving_endpoint_name = f"{catalog}_{db}_endpoint"[:63]
+serving_endpoint_name = f"{catalog}_{db}_endpoint"
 
 client = MlflowClient()
 mlflow.set_registry_uri('databricks-uc')
@@ -75,8 +68,7 @@ auto_capture_config = {
 # MAGIC %md
 # MAGIC 3. Now, you're ready to deploy your model.
 # MAGIC
-# MAGIC **Please be patient.**
-# MAGIC **It may take up to 15 to 30 minutes for Databricks to deploy your model.**
+# MAGIC *Please be patient: It may take up to 15 to 30 minutes for Databricks to deploy your model.*
 
 # COMMAND ----------
 
@@ -95,13 +87,13 @@ displayHTML(f'Your Model Endpoint Serving is now available. Open the <a href="/m
 
 # COMMAND ----------
 
-
 df_split = DataframeSplitInput(columns=["messages"],
                                data=[[{"messages": [{"role": "user", "content": "What is Apache Spark?"},
                                                     {"role": "assistant", "content": "Apache Spark is an open-source data processing engine that is widely used in big data analytics."},
                                                     {"role": "user",
                                                      "content": "Does it support streaming?"}
                                                     ]}]])
+
 w = WorkspaceClient()
 w.serving_endpoints.query(serving_endpoint_name, dataframe_split=df_split)
 
@@ -109,8 +101,6 @@ w.serving_endpoints.query(serving_endpoint_name, dataframe_split=df_split)
 
 # MAGIC %md
 # MAGIC ## Conclusion
-# MAGIC Great job! As you can see, Databricks makes it easy to deploy your own chat model as an API with built-in monitoring. Let's continue.
+# MAGIC Great job! As you can see, Databricks makes it easy to deploy your own chat model as an API with built-in monitoring. 
 # MAGIC
-# MAGIC Now, let's now build a user interface for our chatbot. As part of your workshop AWS account, you have a [Streamlit](https://streamlit.io/) chatbot application deployed to an Amazon EC2 instance. If you're not familiar, Streamlit is a way to quickly turn data scripts into web applications using Python.
-# MAGIC
-# MAGIC Let's head back to Workshop Studio Documentation to see how we can integrate our model endpoint with a web application on AWS
+# MAGIC Next, let's build a user interface for our chatbot. Let's head back to [Workshop Studio](https://prod.workshops.aws/event/dashboard/en-US/workshop/lab3-deploy-your-chat-model/02-streamlit-ui).
