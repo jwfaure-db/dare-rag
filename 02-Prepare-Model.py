d231e2174c850ac80f515ede6dc9507fbf7febfc
wirjo <wirjo@amazon.com>
2024-05-06 14:56:09 +1000

    fix: sqlalchemy version to 2.0.30

--- 02-Prepare-Model.py
+++ 02-Prepare-Model.py
@@ -25,7 +25,7 @@
 
 # COMMAND ----------
 
-# MAGIC %pip install mlflow==2.10.1 lxml==4.9.3 langchain==0.1.5 databricks-vectorsearch==0.22 cloudpickle==2.2.1 databricks-sdk==0.18.0 cloudpickle==2.2.1 pydantic==2.5.2 sqlalchemy
+# MAGIC %pip install mlflow==2.10.1 lxml==4.9.3 langchain==0.1.5 databricks-vectorsearch==0.22 cloudpickle==2.2.1 databricks-sdk==0.18.0 cloudpickle==2.2.1 pydantic==2.5.2 sqlalchemy==2.0.30
 # MAGIC %pip install pip mlflow[databricks]==2.10.1
 # MAGIC dbutils.library.restartPython()
 
